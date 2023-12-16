819ca9beb99468df68b2eab8945fa80c93e3ef85
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-12-16 15:10:14 +0530

    Update_2.3.2

--- src/components/ExpenceItem.jsx
+++ src/components/ExpenceItem.jsx
@@ -1,14 +1,15 @@
 import './ExpenceItem.css';
 
-function ExpenceItem(){
+function ExpenceItem(props){
+
     return (
     
     <div className="expense-item">
-        <div>Expense Items</div>
-        <div className="expense-item__description">Food Rs 10</div>
-    <div className="expense-item__description">Petrol Rs 100</div>
-    <div className="expense-item__description">Location</div>
-    <div className="expense-item__price">Movies Rs 200</div>
+        <div>{props.title}</div>
+    
+    <div className="expense-item__description">{props.date.toISOString()}</div>
+   
+    <div className="expense-item__price">{props.amount}</div>
     
     </div>
     );
