761df4abcf2c1d6d9bcc67ffdab387a9bad3b3ae
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-12-16 16:19:54 +0530

    Update_3.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,60 @@
 import ExpenseItem from './components/ExpenceItem';
 
+
+
 function App() {
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
+
       amount: 94.12,
       date: new Date(2020, 7, 14),
+      details:[ 
+        { detail: 'Toilet Paper' },
+        { detail: 'Monthly expence' }
+      ],
+      
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'TV bill', amount: 799.49, date: new Date(2021, 2, 12), details: [
+    { detail: 'TV bill ' },{ detail: 'Monthly expence' }]
+  },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
+      details: [
+        { detail: 'Car Insurance' },
+        { detail: 'mothly expence' }]
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
+      details: [
+        { detail: 'New Desk for study' },
+        { detail: 'one time ' }]
     },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      
+      {expenses.map((expense) => (
+        <ExpenseItem
+          // key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          details={expense.details}
+
+
+        />
+        
+      ))}
     </div>
   );
 }
