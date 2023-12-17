ae6ce8a150b72b0305c2d8b5003c450f8e02c9f4
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-12-18 01:35:33 +0530

    Update_7.1

--- src/components/ExpenceForm.jsx
+++ src/components/ExpenceForm.jsx
@@ -2,21 +2,25 @@ import React, { useState } from 'react';
 
 import './ExpenceForm.css';
 
-const ExpenceForm = () => {
+const ExpenceForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
   
 
-  const titleChangeHandler = () => {
-    
-    console.log("title")
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+   
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
     
   };
 
-  const amountChangeHandler = () => {
-    console.log("amount")
-  };
-
-  const dateChangeHandler = () => {
-    console.log("date")
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    
   };
 
   const submitHandler = (event) => {
@@ -27,8 +31,11 @@ const ExpenceForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate)
     };
-
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredDate('')
     console.log(expenseData);
+    props.onSaveExpence(expenseData);
   };
 
   return (
@@ -36,11 +43,12 @@ const ExpenceForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
           <input
+            value={enteredAmount}
             type='number'
             min='0.01'
             step='0.01'
@@ -50,6 +58,7 @@ const ExpenceForm = () => {
         <div className='new-expense__control'>
           <label>Date</label>
           <input
+            value={enteredDate}
             type='date'
             min='2019-01-01'
             max='2022-12-31'
@@ -64,5 +73,4 @@ const ExpenceForm = () => {
   );
 };
 
-export default  ExpenceForm; 
-   
+export default ExpenceForm;
