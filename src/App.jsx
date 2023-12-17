88f078d1992b7f8a43275bd0e5667f7ccd707451
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-12-17 17:59:35 +0530

    Update_5.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
-
 // import ExpenseItem from './components/ExpenceItem';
 
 import ExpenseItem from './components/Expence/ExpenceItem';
 
 import React, { useState } from 'react';
 
-
-
-
 function App() {
-
-
-  
-
-  const [expenses, setExpenses] = useState( [
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
-
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      details:[ 
-
-        { detail: 'Toilet Paper' }
-      ],
-      
+      details: [{ detail: 'Toilet Paper' }],
     },
-    { id: 'e2', title: 'TV bill', amount: 799.49, date: new Date(2021, 2, 12), details: [
-
-    { detail: 'TV bill ' }]
-
-  },
+    {
+      id: 'e2',
+      title: 'TV bill',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      details: [{ detail: 'TV bill ' }],
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      details: [
-
-        { detail: 'Car Insurance' }]
+      details: [{ detail: 'Car Insurance' }],
     },
     {
       id: 'e4',
@@ -47,44 +34,39 @@ function App() {
       date: new Date(2021, 5, 12),
       details: [
         { detail: 'New Desk for study' },
-        { detail: 'one time ' }]
+        { detail: 'one time ' },
+      ],
     },
-
-   
   ]);
 
+  const updateExpenseAmountHandler = (expenseId) => {
+    // Find the expense with the given ID
+    const updatedExpenses = expenses.map((expense) => {
+      if (expense.id === expenseId) {
+        // Update the amount to a new value, for example, $100
+        return { ...expense, amount: expense.amount + 100};
+      }
+      return expense;
+    });
 
-  const removeExpenseHandler = (expenseId) => {
-    // Filter out the expense with the given ID
-    const updatedExpenses = expenses.filter((expense) => expense.id !== expenseId);
-
-    // Update the state with the filtered array
+    // Update the state with the modified expenses array
     setExpenses(updatedExpenses);
   };
-
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      
+
       {expenses.map((expense) => (
         <ExpenseItem
-
-          // key={expense.id}
-
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
           details={expense.details}
-
-
-          onRemoveExpense={() => removeExpenseHandler(expense.id)} // Pass the expense ID
-
-
-
+          onRemoveExpense={() => removeExpenseHandler(expense.id)}
+          onUpdateAmount={() => updateExpenseAmountHandler(expense.id)}
         />
-        
       ))}
     </div>
   );
