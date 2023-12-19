8377b5aab9966a932425ca1c08e12b33459ccd66
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-12-20 01:16:34 +0530

    Update_final

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,35 @@ import ExpenseItem from './components/Expence/ExpenceItem';
 import NewExpense from './components/Expence/NewExpence';
 import ExpenceFilter from './components/Expence/ExpenceFilter';
 import React, { useState } from 'react';
+import ExpensesChart from './components/Expence/ExpenceChart';
+
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 
 function App() {
-  const [expenses, setExpenses] = useState([]);
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const [isFormVisible, setIsFormVisible] = useState(false);
 
   const addExpenseHandler = (expenseData) => {
@@ -36,6 +62,7 @@ function App() {
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    
   };
 
   const filteredExpenses = expenses.filter(
@@ -45,17 +72,22 @@ function App() {
     setIsFormVisible(!isFormVisible);
   };
 
+ 
   return (
     <div>
       {isFormVisible ? (
         <NewExpense addExpenseHandler={addExpenseHandler} />
       ) : (
-        <div style={{backgroundColor:'#a892ee',width:'70%',height:'150px', marginLeft:'15%',borderRadius:'20px',justifyContent:'center',alignItems:'center',display:'flex'}}>
+        <div style={{backgroundColor:'#a892ee',width:'70%',marginTop:'30px',height:'150px', marginLeft:'15%',borderRadius:'10px',justifyContent:'center',alignItems:'center',display:'flex'}}>
         <button style={{ backgroundColor: '#40005d', color:'white',paddingTop:'20px',paddingBottom:'20px',paddingLeft:'30px',paddingRight:'30px', borderRadius:'10px', borderBlock:'#40005d'}} onClick={toggleFormVisibility}>Add Expense</button>
         </div>
       )}
       
+      
       <ExpenceFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expence={filteredExpenses || []}/>
+      
+      
       
       {filteredExpenses.length === 0 ? (
         <p style={{ color: '#40005d', fontStyle: 'italic', fontSize: 'larger', marginLeft: '15%', marginTop: '-50px' }}>
